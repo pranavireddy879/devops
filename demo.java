d266565598f77571f31e09856e9fea8e73465390
pranavireddy879 <104255172+pranavireddy879@users.noreply.github.com>
2022-05-09 14:48:56 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>SampleWebApp</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h2>Sample Web Application To Deploy and Test</h2>
+        <br>
+    <div> click <a href="SnoopServlet">Click to Invoke a SnoopServlet</a></div>
+    <br>
+    <br>
+    
+    
+    </body>
+</html>
